06bb835ba9c9e02e45bcfe7793ee2d8aec06b261
David Stroud <david@greymatter-ag.com>
2013-10-31 11:28:26 -0400

    began iOS 7 implementation of app

--- Card.m
+++ Card.m
@@ -8,19 +8,12 @@
 
 #import "Card.h"
 
+@interface Card()
+
+@end
+
 @implementation Card
 
--(int)match:(NSArray *)otherCards;
-{
-    int score = 0;
-    
-    for (Card *card in otherCards) {
-        if ([card.contents isEqualToString:self.contents]) {
-            score = 1;
-        }
-    }
-    
-    return score;
-}
+
 
 @end
