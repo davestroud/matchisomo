a3dbd998cc36a6b11f9497b6b129b93aa8d60c3f
David Stroud <david@greymatter-ag.com>
2013-10-31 12:27:02 -0400

    added deck as a new class

--- Matchismo/Model/Deck.h
+++ Matchismo/Model/Deck.h
@@ -7,12 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Card.h"
 
 @interface Deck : NSObject
 
--(void)addCard:(Card *)card atTop:(BOOL) atTop;
+- (void)addCard:(Card *card) atTop:(bool)atTop;
 
--(Card *)drawRandomCard;
+- (Card *)drawRandomCard;
+
+
+
+
 
 @end
+
+
