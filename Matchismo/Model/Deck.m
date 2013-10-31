9a0e2da8038899ce1d3694f6b5077688cb5a90b3
David Stroud <david@greymatter-ag.com>
2013-10-31 13:19:22 -0400

    added nsmutablearray for the needed heap allocation

--- Matchismo/Model/Deck.m
+++ Matchismo/Model/Deck.m
@@ -8,24 +8,39 @@
 
 #import "Deck.h"
 
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards; // of card
+
 @end
 
 @implemenation Deck
 
+- (NSMutableArray *)cards
+{
+    return _cards;
+}
 
 
 
 
 
 
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    
+    if (atTop)  {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
 
+- (void)addCard:(Card *)card
+{
+    [self addCard:card atTop:NO];
+}
 
-
-
-
-
-
-
+- (Card *)drawRandomCard {}
 
 @end
 
